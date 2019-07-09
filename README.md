b2c3d04a0da91c7b1ed64c69f6e75eeb374e08c6
Hasimy <ahmadhasyimi674@gmail.com>
2019-07-09 11:40:04 +0700

    MINOR CHANGES: Updated LOGOUT Method

--- README.md
+++ README.md
@@ -2,13 +2,14 @@
 
 Bookinglab-User adalah sebuah website intra-sekolah [SMK Telkom Malang](https://smktelkom-mlg.sch.id/) yang digunakan oleh warga sekolah supaya dapat memudahkan mereka untuk memesan Laboratorium Komputer yang tersedia di SMK Telkom Malang.
 
-#Status Projek
+# Status Projek
 
-[![Lisensi GitHub](https://img.shields.io/badge/license-MIT-blue.svg)](https://raw.githubusercontent.com/hasimy-as/Bookinglab-User/master/LICENSE)
+[![Lisensi GitHub](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://raw.githubusercontent.com/hasimy-as/Bookinglab-User/master/LICENSE)
 [![Versi npm](https://img.shields.io/npm/v/npm.svg)](https://www.npmjs.com/)
 [![Status dependencies](https://img.shields.io/hackage-deps/v/lens.svg)](https://github.com/hasimy-as/Bookinglab-User)
 
 ## Versi 1.0.0
+
 Versi bisa saja berubah bilamana ada update projek.
 
 # Penggunaan
@@ -24,7 +25,7 @@ Ketiga, ketik perintah (npm start) untuk menginisialisasi aplikasi.
 
 ```
 
-## Dilisensikan di bawah Lisensi Apache-2.0
+## Dilisensikan di bawah Lisensi [Apache-2.0](https://opensource.org/licenses/Apache-2.0/)
 
 Happy coding!
 ~Hasimy
--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 //import dependencies & settings
-const express = require('express'),
-    session = require('express-session'),
-    db = require('./mysql.js'),
+const express = require("express"),
+    session = require("express-session"),
+    db = require("./mysql.js"),
     // path = require('path'),
-    bodyParser = require('body-parser'),
+    bodyParser = require("body-parser"),
     app = express(),
     port = 5000;
 
-app.set('view engine', 'ejs');
-app.set(express.static('public'));
-app.use('/public', express.static('public'));
+app.set("view engine", "ejs");
+app.set(express.static("public"));
+app.use("/public", express.static("public"));
 app.use(bodyParser.json());
 app.use(
     bodyParser.urlencoded({
@@ -18,71 +18,72 @@ app.use(
 );
 app.use(
     session({
-        secret: 'secret',
+        secret: "secret",
         resave: true,
         saveUninitialized: true
     })
 );
 
 //rendernya
-app.get('/', (req, res) => {
-    res.render('login');
+app.get("/", (req, res) => {
+    res.render("login");
 });
 
-app.post('/authlogin', (req, res) => {
+app.post("/authlogin", (req, res) => {
     var email = req.body.email;
     var password = req.body.password;
-    const sql = 'SELECT * FROM user WHERE email_user = ? AND password = ?';
+    const sql = "SELECT * FROM user WHERE email_user = ? AND password = ?";
     if (email && password) {
         db.query(sql, [email, password], function (err, rows) {
             if (err) throw err;
             else if (rows.length > 0) {
                 req.session.loggedin = true;
                 req.session.email = email;
-                res.redirect('/home');
+                res.redirect("/home");
             } else {
-                res.write('Kredensial anda salah!');
+                res.write("Kredensial anda salah!");
             }
             res.end();
         });
     }
 });
 
-app.get('/home', (req, res) => {
+app.get("/home", (req, res) => {
     if (req.session.loggedin) {
-        res.render('home');
+        res.render("home");
+
     } else {
-        res.redirect('/pelanggaran');
+        res.redirect("/pelanggaran");
     }
     res.end();
 });
 
-app.get('/profil', (req, res) => {
-    res.render('profil');
+app.get("/profil", (req, res) => {
+    res.render("profil");
 });
 
-app.get('/lab', (req, res) => {
-    res.render('laboratorium');
+app.get("/lab", (req, res) => {
+    res.render("laboratorium");
 });
 
-app.get('/jadwal', (req, res) => {
-    res.render('jadwal');
+app.get("/jadwal", (req, res) => {
+    res.render("jadwal");
 });
 
-app.get('/logout', (req, res) => {
-    if (req.session.loggedin) {
+app.get("/logout", (req, res) => {
+    if (req.session.loggedin === true) {
         req.session.loggedin = false;
-        res.redirect('/');
+        res.redirect("/");
     }
     res.end();
 });
 
-app.get('/pelanggaran', (req, res) => {
-    res.render('logindulu');
+app.get("/pelanggaran", (req, res) => {
+    res.render("logindulu");
 });
 
-app.get('*', (req, res) => {
-    res.render('404');
+app.get("*", (req, res) => {
+    res.render("404");
 });
 
 //declare port
